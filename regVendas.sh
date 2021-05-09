d3ee86945f65844dcd9480e993f800d6b3decff3
Leonardo araujo <lcostaaraujo9@gmail.com>
2021-05-09 00:17:09 -0300

    Suporte a titulo

--- regVendas.sh
+++ regVendas.sh
@@ -11,7 +11,9 @@ ARQ="$WORK_DIR/$(date +%F).txt"
 
 [[ ! -d "$WORK_DIR" ]] && mkdir "$WORK_DIR"
 [[ ! -e "$ARQ" ]] && touch "$ARQ"
-[[ -z "$TITLE" ]] && read -p "Primeiro defina o titulo o nome do seu negócio : " TITLE && export TITLE
+[[ -z "$TITLE" ]] && read -p "Nome do seu negócio : " TITLE ; \
+			export TITLE ; \
+			echo "export TITLE=\"$TITLE\"" >> $HOME/.bashrc ;
 
 # INICIO
 
@@ -19,18 +21,18 @@ while : ; do
 
     form=$(zenity --width=400 --height=200 --forms --text "Produto e Preço" \
      --title="$TITLE" \
-     --add-entry="Produto" --add-entry="Preço" --separator="  --   R$") ;
-    
+     --add-entry="Produto" --add-entry="Preço" --separator="  --   R$")
+
     if  [[ $form ]] ; then # Nao for vazio
         echo "[ $(date +%c) ] $form" >> $ARQ ;
         elif [[ $(zenity --list --text "Fechamento" \
 					--checklist \
 					--column "" \
 					--column="Ação" \
-					FALSE "Exibir Resumo") ]] ; then 
+					FALSE "Exibir Resumo") ]] ; then
         zenity --info --text="AQUI vem o resumo" ; else
         exit 0;
     fi
-        
+
 done
 # Remover linhas duplicadas nos arquivos de registro
