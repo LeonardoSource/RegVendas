7fab2c05969641c2bc935c5ead199109a7784e07
Leonardo araujo <lcostaaraujo9@gmail.com>
2021-07-13 20:59:03 -0300

    Exibir outros totais

--- regVendas.sh
+++ regVendas.sh
@@ -7,9 +7,8 @@
 
 # Configuração inicial
 WORK_DIR="$HOME/RegVendas"
-ARQ="$WORK_DIR/$(date +%F).txt"
+ARQ="$WORK_DIR/$(date +$U)_$(date +%F).txt"
 TITLE="Vendas"
-SOMA=0
 
 [[ ! -d "$WORK_DIR" ]] && mkdir "$WORK_DIR"
 [[ ! -e "$ARQ" ]] && touch "$ARQ"
@@ -22,27 +21,48 @@ SOMA=0
 
 while : ; do
 
+    SOMA=0
     form=$(zenity --width=400 --height=200 --forms --text "Produto e Preço" \
      --title="$TITLE" \
      --add-entry="Produto" --add-entry="Preço" --separator="  --   R$")
 
     if  [[ $form ]] ; then # Nao for vazio
+        
         echo "[ $(date +%c) ] $form" >> $ARQ ;
-        elif [[ $(zenity --list --text "Fechamento" \
-					--checklist \
-					--column "" \
-					--column="Ação" \
-					FALSE "Exibir Total ") ]] ; then
         
-        # Faz a soma de todos os arquivos fora das pastas
-        for  a in "$WORK_DIR/*" ; do
-            [[  ! -d $a ]] && SOMA=$(($(awk  -F 'R\\$' 'BEGIN {total=0} {total+=$2} END {print total}' $a)+$SOMA))
-        done;
-        
-        #exibe ao total
-        zenity --info --text="O Total Vendido ate o momento registrado neste programa e $SOMA" ; else
-        
-        exit 0;
+    else
+        case $(zenity --list --text "Fechamento" \
+                --radiolist \
+                --column "" \
+                --column="EXIBIR" \
+                FALSE "Total Semanal" \
+                FALSE "Total do Dia" \
+                FALSE "Total do Mês" \
+                FALSE "Total Registrado" \
+                FALSE "Última Venda") in
+                
+                "Total Semanal")
+                
+                        # Faz a soma de todos os arquivos fora das pastas
+                        for  a in "$WORK_DIR/*" ; do
+                            [[  ! -d $a ]] && SOMA=$(($(awk  -F 'R\\$' 'BEGIN {total=0} {total+=$2} END {print total}' $a)+$SOMA))
+                        done;
+                        
+                        #exibe ao total
+                        zenity --info --text="O Total Vendido ate o momento registrado neste programa e $SOMA" ;;
+                        
+                "Total do Dia")
+                    ;;
+                "Total do Mês")
+                    ;;
+                "Total  Registrado")
+                    ;;
+                "Última Venda")
+                    ;;
+               *)
+                
+                exit 0;
+        esac
     fi
 
 done
